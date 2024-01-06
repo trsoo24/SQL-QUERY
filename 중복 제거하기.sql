1eacbcdce3ab3fd63235a051b1c3643987287cab
trsoo24 <122004333+trsoo24@users.noreply.github.com>
2024-01-06 16:12:05 +0900

    Create 중복 제거하기.sql

---
+++ 중복 제거하기.sql
@@ -0,0 +1,12 @@
+--   NAME	        TYPE	      NULLABLE
+-- ANIMAL_ID  	VARCHAR(N)	   FALSE
+-- ANIMAL_TYPE	VARCHAR(N)	   FALSE
+-- DATETIME	     DATETIME      FALSE
+-- INTAKE_CONDITION	VARCHAR(N) FALSE
+-- NAME	        VARCHAR(N)	   TRUE
+-- SEX_UPON_INTAKE	VARCHAR(N) FALSE
+
+-- 동물 보호소에 들어온 동물의 이름은 몇 개인지 조회하는 SQL 문을 작성해주세요. 이때 이름이 NULL인 경우는 집계하지 않으며 중복되는 이름은 하나로 칩니다.
+
+SELECT COUNT(DISTINCT NAME) FROM ANIMAL_INS -- DISTINCT NAME 은 NAME 내 중복 제거
+WHERE NAME IS NOT NULL;
