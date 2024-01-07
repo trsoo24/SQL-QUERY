1fd4bfd91ae11306f0aa959e0a65d5c7fa7d249e
trsoo24 <122004333+trsoo24@users.noreply.github.com>
2024-01-07 23:52:29 +0900

    중복 제거하기&합 구하기&NULL 찾기.sql

--- 중복 제거하기&합 구하기.sql
+++ 중복 제거하기&합 구하기.sql
@@ -14,3 +14,8 @@ WHERE NAME IS NOT NULL;
 -- 동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문을 작성해주세요.
 
 SELECT COUNT(ANIMAL_ID) FROM ANIMAL_INS;
+
+-- 동물 보호소에 들어온 동물 중, 이름이 있는 동물의 ID를 조회하는 SQL 문을 작성해주세요. 단, ID는 오름차순 정렬되어야 합니다.
+
+SELECT ANIMAL_ID FROM ANIMAL_INS
+WHERE NAME != 'NULL';
